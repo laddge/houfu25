b9309efb0a78441ca38c7c245052f5d837233950
Laddge <dev.laddge@gmail.com>
2025-01-01 01:57:33 +0900

    feat: add header and footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,14 @@
 	let { children } = $props();
 </script>
 
+<header class="navbar bg-base-200">
+  <h1 class="btn btn-ghost text-xl">
+    2025年の抱負
+  </h1>
+</header>
 <main class="container mx-auto p-4">
   {@render children()}
 </main>
+<footer class="text-center p-4 text-xs">
+  &copy; 2025 Laddge
+</footer>
